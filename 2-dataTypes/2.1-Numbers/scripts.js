e05ec006142ec2ebfdf8229495a06013ce55165a
Luiz <luis231103@gmail.com>
2022-02-07 13:28:27 -0300

    First Commit

---
+++ 2-dataTypes/2.1-Numbers/scripts.js
@@ -0,0 +1,20 @@
+// console.log(typeof 5)
+// console.log(typeof -8)
+// console.log(typeof 3.25)
+
+//Aritmética
+
+console.log(5 * 5)
+console.log(5 / 5)
+console.log(5 + 5)
+console.log(typeof (5 - 5))
+
+//Traz apenas o resto, se for exata traz 0 se não traz o 1
+console.log(89 % 2)
+console.log(10 % 2)
+
+//Special Numbers infinity -infinity NaN São numeros porém não são numeros kkk
+
+console.log(typeof Infinity)
+console.log(typeof -Infinity)
+console.log(typeof NaN)
