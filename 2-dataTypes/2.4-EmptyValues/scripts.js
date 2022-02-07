8970944b83b8ae49c12dde58d0fff10810d8d4eb
Luiz <luis231103@gmail.com>
2022-02-07 13:40:03 -0300

    EmptyValues e Coversão de tipo automática

---
+++ 2-dataTypes/2.4-EmptyValues/scripts.js
@@ -0,0 +1,15 @@
+console.log(undefined)
+
+let lucas = null
+let luiz 
+
+console.log(luiz)
+console.log(lucas)
+
+// Conversão automatica de tipo
+
+console.log("1" + 3)// no + há uma semelhança (ambiguidade) entre os parâmetros de concatenação e soma, dando prioridade a concatenação.
+console.log("1" - 3)// já aqui não
+
+console.log("str" * "str")
+
