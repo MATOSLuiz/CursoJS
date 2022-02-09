6db2c1cbdf060d96df290d381b573c3bf0e63d25
Luiz <luis231103@gmail.com>
2022-02-09 18:29:24 -0300

    if else if e else

---
+++ 3-programmingStructures/3.3-ControlStructure/scripts.js
@@ -0,0 +1,17 @@
+//if
+
+let casa1 = "Grifinória";  
+let casa2 = "Sonserina";  
+
+let atributo1 = "Coragem";
+let atributo2 = "Ambição";
+
+let pessoa = "Ambição";
+
+if(pessoa == atributo1) {
+    console.log("Você é da " + casa1);
+} else if(pessoa == atributo2) {
+    console.log("Você é da " + casa2);
+} else {
+    console.log("Você é trouxa!");
+}
