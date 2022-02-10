a611fa830d413f411227594159b98a88257e94f4
Luiz <luis231103@gmail.com>
2022-02-10 15:08:12 -0300

    Utilização do break para parar o looping!

--- 3-programmingStructures/3.4-Loop/scripts.js
+++ 3-programmingStructures/3.4-Loop/scripts.js
@@ -17,7 +17,15 @@ do {
 } while(y >= 0);
 
 //for ; while mais compacto argumento1 = variavel arg2 = condição arg3 = incremento ou decremento
+let nome = "Carlos";
 
-for(let z = 0; z < 100; z = z + 10) {
-    console.log(z);
+for (let z = 0; z < 100; z = z + 10) {
+    if(z == 20) {
+        nome = "Luiz";
+    }
+    if(z == 80 && nome == "Luiz") {
+        console.log(`Feito o nome é ${nome} e Z é ${z}`)
+        break //break é utilizado para abortar um bloco de codigo no exemplo um for.
+    }
+    console.log(z)
 }
