4a79b2ebe49101f195f31333ccfe8866d63e53f2
Luiz <luis231103@gmail.com>
2022-02-14 20:38:58 -0300

    Um pouco mais sobre escopo

--- 3-programmingStructures/3.5-Functions/scripts.js
+++ 3-programmingStructures/3.5-Functions/scripts.js
@@ -8,6 +8,10 @@ function mostraNome(nome) {
 
 mostraNome("Luiz");
 
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
 //função com return
 
 const olaMundo = function(inicio) {
@@ -18,9 +22,47 @@ const olaMundo = function(inicio) {
 
 console.log(olaMundo(true));
 
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
 
 const sub = function(a,b) {
     return a - b;
 }
 
 console.log(sub(15,1))
+
+//escopo da função
+
+let x = 25; //escopo global
+
+const numero = function(){
+    let x = 15;//escopo local
+    console.log(x);
+}
+
+console.log(x)
+
+numero();
+
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
+//mais sobre escopo, qualquer bloco de código cria é um novo escopo, não só as funções
+
+let y = 89;
+
+for(let y = 30; y <= 50; y++) {
+    console.log(y);
+}
+
+console.log("\n");
+console.log("\n");
+console.log("\n");
+
+
+
+
+
